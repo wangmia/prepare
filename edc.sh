115a299ed64683c8535f39199874583e387bec87
edz <1125847214@qq.com>
2020-01-11 20:36:16 +0800

    综合练习

---
+++ edc.sh
@@ -0,0 +1,11 @@
+ mkdir example
+ mv ./ex01.c example/example.c
+ mkdir practice 
+ mv ./ex02.c practice/ex02.c
+ mr ex03.c
+ touch README.md 
+ echo "## example/example.c">README.md
+ echo "判断是基数还是偶数">>README.md
+ echo "## practice/practice.c">>README.md
+ echo "比较两个数大小">>README.md
+
